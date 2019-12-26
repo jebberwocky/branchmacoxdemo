7b9541336ae7fe59974032e1c075110ab29e5762
jeff_jxcto <jeff.liu@jxcto.com>
2019-12-26 17:08:15 +0800

    Initial Commit

---
+++ whereyogi-macos/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  whereyogi-macos
+//
+//  Created by Jeff Liu on 12/26/19.
+//  Copyright © 2019 Jeff Liu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ whereyogi-macos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  whereyogi-macos
+//
+//  Created by Jeff Liu on 12/26/19.
+//  Copyright © 2019 Jeff Liu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

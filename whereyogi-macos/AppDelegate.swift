645d47f22b9637bab59c1712e1a750b8a0e3967c
jeff_jxcto <jeff.liu@jxcto.com>
2019-12-30 14:45:48 +0800

    init

--- whereyogi-macos/AppDelegate.swift
+++ whereyogi-macos/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import Cocoa
+import Branch
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
@@ -15,6 +16,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+          // Create a Branch configuration object with your key:
+          let configuration = BranchConfiguration(key: "key_live_igTdpT9cigREKYlQt5abZeocrwkCS0mo")
+
+          // Start Branch:
+        Branch.sharedInstance.start(with: configuration)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
--- whereyogi-macos/ViewController.swift
+++ whereyogi-macos/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import Cocoa
+import Branch
 
 class ViewController: NSViewController {
-
+    
+    
+    
+    @IBOutlet weak var sendEButton: NSButton!
+    @IBOutlet weak var sendCButton: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +27,41 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    @IBAction func sendCustomEvent(button: NSButton)
+    {
+        Branch.sharedInstance.logEvent(
+            BranchEvent.customEvent(withName: "MAC_JEFF_EVENT"));
+        let alert: NSAlert = NSAlert()
+        alert.messageText = "MAC_JEFF_EVENT Event Sent"
+        alert.informativeText = "MAC_JEFF_EVENT Event Sent"
+        alert.addButton(withTitle:"OK")
+        let res = alert.runModal()
+    }
 
-
+    @IBAction func sendCommerceEvent(button: NSButton)
+    {
+        let event = BranchEvent.standardEvent(.purchase)
+        event.transactionID    = "tx-12344555"
+        event.currency         = .USD
+        event.revenue          = 12.70
+        event.shipping         = 10.20
+        event.tax              = 2.50
+        event.coupon           = "coupon_code"
+        event.affiliation      = "store_affiliation"
+        event.eventDescription = "Shopper made a purchase."
+        event.searchQuery      = "Fashion Scarf"
+        event.customData       = [
+            "Item_Color": "Red",
+            "Item_Size":  "Large"
+        ]
+        Branch.sharedInstance.logEvent(event)
+        
+        let alert: NSAlert = NSAlert()
+        alert.messageText = "Commerce Event Sent"
+        alert.informativeText = "Commerce Event Sent"
+        alert.addButton(withTitle:"OK")
+        let res = alert.runModal()
+    }
 }
 
